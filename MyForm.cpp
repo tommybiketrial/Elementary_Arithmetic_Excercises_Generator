c4f206179c0daef0f0f28038ea62b8a4c3a0e025
tommybiketrial <tommy@fotogallery.hk>
2021-07-08 17:36:54 +0800

    Release Version x64
    Hide Console at startup

--- MyForm.cpp
+++ MyForm.cpp
@@ -7,6 +7,7 @@ void main() { //removed array<String^>^ args
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	Windowsform07062021::MyForm form;
+	FreeConsole();
 	Application::Run(% form);
 }
 
